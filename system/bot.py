4c11f00f0069b54ff216fd99f3e42ba64516a2f0
Cealestus <meisler@live.ca>
2018-03-05 18:00:25 -0600

    Updated bot to use infinite threads.
    Don't know if this is a good idea or bad...

--- system/bot.py
+++ system/bot.py
@@ -24,34 +24,43 @@ runBot = True
 runBotLock = threading.Lock()
 requestQueue = queue.Queue()
 
+def playAudio(sound):
+    try:
+        wf = wave.open(os.environ['HOMEPATH'] + '\\Desktop\ChatDrums\\' + sound + '.wav', 'rb')
+        stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
+                        channels=wf.getnchannels(),
+                        rate=wf.getframerate(),
+                        output=True)
+        data = wf.readframes(cfg.CHUNK)
+        while len(data) > 0:
+            stream.write(data)
+            data = wf.readframes(cfg.CHUNK)
+        stream.stop_stream()
+        stream.close()
+    except FileNotFoundError:
+        chat(twitchSocket, 'Error: File not found')
+    
+
 def playFromQueue():
     while True:
         if requestQueue.qsize() != 0:
-            try:
-                sound = requestQueue.get(block=True)
-                wf = wave.open(os.environ['HOMEPATH'] + '\\Desktop\ChatDrums\\' + sound + '.wav', 'rb')
-                stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
-                                channels=wf.getnchannels(),
-                                rate=wf.getframerate(),
-                                output=True)
-                data = wf.readframes(cfg.CHUNK)
-                while len(data) > 0:
-                    stream.write(data)
-                    data = wf.readframes(cfg.CHUNK)
-                stream.stop_stream()
-                stream.close()
-            except FileNotFoundError:
-                chat(twitchSocket, 'Error: File not found')
+            soundName = requestQueue.get(block=True)
+            newThread = threading.Thread(target=playAudio(sound=soundName))
+            newThread.daemon = True
+            newThread.start()
 
 def requestFromChat():
     global runBot
+    print("Starting thread")
     while True:
         response = twitchSocket.recv(1024).decode("utf-8")
+        print(str(response))
         if response == "PING :tmi.twitch.tv\r\n":
             twitchSocket.send("PONG :tmi.twitch.tv\r\n".encode("utf-8"))
         else:
             username = re.search(r"\w+", response).group(0)
             message = CHAT_MSG.sub("", response)
+            print(message)
             lowerCase = message.lower()
             splitMessage = lowerCase.split()
             if len(splitMessage) == 1:
@@ -67,6 +76,7 @@ def requestFromChat():
                         if command in cfg.commands:
                             requestQueue.put(command)
 
+
 requestThread = threading.Thread(target=requestFromChat)
 requestThread.daemon = True
 requestThread.start()
@@ -75,9 +85,6 @@ playThread = threading.Thread(target=playFromQueue)
 playThread.daemon = True
 playThread.start()
 
-playThread2 = threading.Thread(target=playFromQueue)
-playThread2.daemon = True
-playThread2.start()
 
 chat(twitchSocket, 'Started drumbot')
                         
